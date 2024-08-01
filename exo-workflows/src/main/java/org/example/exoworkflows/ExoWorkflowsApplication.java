524c4c11eee75131d6b7c28e0a9a3e8c54fa2972
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-08-01 12:36:08 +0200

    Initial commit

---
+++ exo-workflows/src/main/java/org/example/exoworkflows/ExoWorkflowsApplication.java
@@ -0,0 +1,13 @@
+package org.example.exoworkflows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoWorkflowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExoWorkflowsApplication.class, args);
+    }
+
+}
---
+++ exo-workflows/src/test/java/org/example/exoworkflows/ExoWorkflowsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exoworkflows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoWorkflowsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
